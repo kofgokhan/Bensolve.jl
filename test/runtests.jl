437c5cf9be4bc7bbb2e0cd19e44d833cfdea5a67
Gökhan Kof <kofgokhan@gmail.com>
2025-06-16 19:42:27 +0300

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,31 +5,32 @@ include("examples/example_utils.jl")
 
 @testset "Bensolve.jl using *.vlp files" begin
     for i = 1:6
-        0 == solve("examples/ex0$(i).vlp")
+        solve("examples/ex0$(i).vlp")
     end
 end
 
 @testset "Bensolve.jl using functional interface" begin
     for i = 1:6
-        filename = "test/examples/ex0$(i).vlp"
+        filename = "examples/ex0$(i).vlp"
 
-        opt_dir, m, n, nz, q, nzobj, cone... = _extract_problem_info_from_file(fname)
+        opt_dir, m, n, q, cone... = _extract_problem_info_from_file(filename)
 
         if isempty(cone)
-            P = _extract_objective_matrix_from_file(fname, q, n)
-            B = _extract_constraint_matrix_from_file(fname, m, n)
-            a, b = _extract_row_bounds(fname, m)
-            l, s = _extract_col_bounds(fname, n)
+            P = _extract_objective_matrix_from_file(filename, q, n)
+            B = _extract_constraint_matrix_from_file(filename, m, n)
+            a, b = _extract_row_bounds(filename, m)
+            l, u = _extract_col_bounds(filename, n)
             molp_solve(P, B, a, b, l, s, opt_dir)
         else
-            ctype, C, c = cone
-            P = _extract_objective_matrix_from_file(fname, q, n)
-            B = _extract_constraint_matrix_from_file(fname, m, n)
-            a, b = _extract_row_bounds(fname, m)
-            l, s = _extract_col_bounds(fname, n)
-            C = _extract_generator_matrix_from_file(fname, q, n_gen)
-            c = _extract_duality_vec(fname, q)
-            vlp_solve(P, B, a, b, l, s, opt_dir, ctype, C, c)
+            ctype, n_gen, nzgen = cone
+            P = _extract_objective_matrix_from_file(filename, q, n)
+            B = _extract_constraint_matrix_from_file(filename, m, n)
+            a, b = _extract_row_bounds(filename, m)
+            l, u = _extract_col_bounds(filename, n)
+            C = _extract_generator_matrix_from_file(filename, q, n_gen)
+            c = _extract_duality_vec(filename, q)
+            cone_gen = (lowercase(ctype) == "cone" ? 0 : 1)
+            vlp_solve(P, B, a, b, l, u, C, c, opt_dir, cone_gen)
         end
     end
 end
