dea9bf9a8ca2d3aa7f029188bbeaa69566cefcd3
Gökhan Kof <kofgokhan@gmail.com>
2025-06-21 17:26:27 +0300

    Update list2d loading

--- src/functional_interface.jl
+++ src/functional_interface.jl
@@ -1,45 +1,19 @@
-function _get_vlp(
-    P::AbstractMatrix,
-    B::AbstractMatrix,
-    a::AbstractVector,
-    b::AbstractVector,
-    l::AbstractVector,
-    s::AbstractVector,
-    opt_dir::Int = 1,
-    cone_gen::cone_gen_type = DEFAULT;
-    Y_or_Z::AbstractMatrix = Float64[],
-)
-    m, n, q = size(B)..., size(P, 1)
-    n_gen = size(Y_or_Z, 2)
-    A_ext = _matrix2list2d([B zeros(m, q); P -I(q)])
-    nz, nzobj = nnz(sparse(B)), nnz(sparse(P))
-    gen = cone_gen == DEFAULT ? C_NULL : Y_or_Z
-    rows = _vecs2rows(a, b)
-    cols = _vecs2cols(l, s)
-
-    vlp = Ref(
-        vlptype(
-            Ptr{list2d}(A_ext),           # A_ext
-            Ptr{boundlist}(rows),         # rows
-            Ptr{boundlist}(cols),         # cols
-            opt_dir,                      # optdir
-            cone_gen_type,                # cone_gen
-            Ptr{Cdouble}(gen),            # gen
-            Ptr{Cdouble}(zeros(q)),       # c
-            Clong(nz),                    # nz
-            Clong(nzobj),                 # nzobj
-            lp_idx(n),                    # n
-            lp_idx(m),                    # m
-            lp_idx(q),                    # q
-            lp_idx(n_gen),                 # n_gen
-        ),
-    )
-    return vlp
-end
-
 function _matrix2list2d(A::AbstractMatrix{<:Real})
     I, J, V = findnz(sparse(A))
+    idx1 = lp_idx.(I)
+    idx2 = lp_idx.(J)
+    data = Cdouble.(V)
 
+    l2d = list2d(Csize_t(length(data)), pointer(idx1), pointer(idx2), pointer(data))
+
+    return l2d, (idx1, idx2, data)
+end
+
+function _matrix2list2d(
+    I::AbstractVector{Int}, 
+    J::AbstractVector{Int}, 
+    V::AbstractVector{<:Real}, 
+)
     idx1 = lp_idx.(I)
     idx2 = lp_idx.(J)
     data = Cdouble.(V)
@@ -99,8 +73,16 @@ function vlp_solve(
     nz, nzobj = sum(B .!= 0), sum(P .!= 0)
 
     # Building A_ext for vlptype
-    A_ext = [B zeros(m, q); -P LinearAlgebra.I(q)]
-    l2d, l2d_ref = _matrix2list2d(A_ext)
+    # A_ext = [B zeros(m, q); -P LinearAlgebra.I(q)]
+    # l2d, l2d_ref = _matrix2list2d(A_ext)
+    I_B, J_B, V_B = findnz(sparse(B))
+    I_P, J_P, V_P = findnz(sparse(-P))
+    I_P .+= m
+    I_I, J_I, V_I = (m+1):(m+q), (n+1):(n+q), fill(1.0, q)
+    I = vcat(I_B, I_P, I_I)
+    J = vcat(J_B, J_P, J_I)
+    V = vcat(V_B, V_P, V_I)
+    l2d, l2d_ref = _matrix2list2d(I, J, V)
 
     # Building the rows and cols for vlptype
     rows, row_refs = _vecs2boundlist(a, b)
@@ -150,6 +132,7 @@ function vlp_solve(
             sol = _sol_init(vlp_ref, opt)
             # Prepare to solve
             lp_init(vlp_ref)
+            # elapsed_time = @elapsed ret = alg(sol, vlp_ref, opt) # need to handle when ret != 0
             elapsed_time = @elapsed ret = alg(sol, vlp_ref, opt) # need to handle when ret != 0
             if ret > 0
                 write_log_file(
