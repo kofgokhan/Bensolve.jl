6953e9f7c6721431ba12d0178b501b4b711c49e7
Gökhan Kof <kofgokhan@gmail.com>
2025-06-21 17:34:34 +0300

    Update test.jl

--- test/test.jl
+++ test/test.jl
@@ -8,7 +8,9 @@ using Bensolve
 
 include("examples/example_utils.jl")
 
-filename = "test/examples/ex01.vlp"
+filename = "test/examples/ex06.vlp"
+
+status, upper_img, lower_img, _ = Bensolve.solve(filename)
 
 function test_problem(filename)
     opt_dir, m, n, q, cone... = _extract_problem_info_from_file(filename)
@@ -19,59 +21,39 @@ function test_problem(filename)
     model = Model(Bensolve.Optimizer)
     @variable(model, l[i] <= x[i=1:n] <= u[i])
     @constraint(model, a .<= B * x .<= b)
-    sense = opt_dir == 1 ? MOI.MIN_SENSE : MOI:MAX_SENSE
+    sense = opt_dir == 1 ? MOI.MIN_SENSE : MOI.MAX_SENSE
     @objective(model, sense, P * x)
     if !isempty(cone)
         ctype, n_gen = cone
         C = _extract_generator_matrix_from_file(filename, q, n_gen)
         c = _extract_duality_vec(filename, q)
-        MOI.set(model, Bensolve.OrderingCone, C)
-        MOI.set(model, Bensolve.DualityVector, c)
-        MOI.set(model, Bensolve.ConeType, if ctype == "CONE"
-            Bensolve.CONE
-        elseif ctype == "DUALCONE"
-            Bensolve.DUALCONE
+        MOI.set(model, Bensolve.OrderingCone(), C)
+        MOI.set(model, Bensolve.DualityVector(), c)
+        MOI.set(model, Bensolve.ConeType(), if lowercase(ctype) == "cone"
+            0
+        elseif lowercase(ctype) == "dualcone"
+            1
         else
-            Bensolve.DEFAULT
+            2
         end)
     end
     optimize!(model)
-    return
+    X = [value.(x, result=i) for i = 1:result_count(model)]
+    Y = [objective_value(model, result=i) for i = 1:result_count(model)]
+    return X, Y
 end
 
-function test_problem(filename)
-    opt_dir, m, n, q, cone... = _extract_problem_info_from_file(filename)
-    P = _extract_objective_matrix_from_file(filename, q, n)
-    B = _extract_constraint_matrix_from_file(filename, m, n)
-    a, b = _extract_row_bounds(filename, m)
-    l, u = _extract_col_bounds(filename, n)
-    if !isempty(cone)
-        ctype, C, c = cone
-        MOI.set(model, Bensolve.OrderingCone, C)
-        MOI.set(model, Bensolve.DualityVector, c)
-        MOI.set(model, Bensolve.ConeType, if ctype == "CONE"
-            Bensolve.CONE
-        elseif ctype == "DUALCONE"
-            Bensolve.DUALCONE
-        else
-            Bensolve.DEFAULT
-        end)
-    end
-    optimize!(model)
-    return
-end
+X, Y = test_problem("test/examples/ex06.vlp")
 
-test_problem("test/examples/ex01.vlp")
-
-filename = "test/examples/ex05.vlp"
 opt_dir, m, n, q, cone... = _extract_problem_info_from_file(filename)
 P = _extract_objective_matrix_from_file(filename, q, n)
 B = _extract_constraint_matrix_from_file(filename, m, n)
 a, b = _extract_row_bounds(filename, m)
 l, u = _extract_col_bounds(filename, n)
-ctype, n_gen, nzgen = cone
+ctype, n_gen = cone
 C = _extract_generator_matrix_from_file(filename, q, n_gen)
 c = _extract_duality_vec(filename, n_gen)
 
-cone_gen = (lowercase(ctype) == "cone" ? 1 : 2)
-Bensolve.vlp_solve(P, B, a, b, l, u, C, c, 1, cone_gen)
+cone_gen = if lowercase(ctype) == "cone" 0 elseif lowercase(ctype) == "dualcone" 1 else 2 end
+
+status, upper_img, _ = Bensolve.vlp_solve(P, B, a, b, l, u, C, c, opt_dir, cone_gen);
